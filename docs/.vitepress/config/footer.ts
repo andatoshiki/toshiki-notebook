ea5dd35c7b2541da84caaded8cdcbe0b864e7248
andatoshiki <101481353+andatoshiki@users.noreply.github.com>
2023-03-02 22:48:05 +0800

    chore: make prettier ignore script folder when lint

--- docs/.vitepress/config/footer.ts
+++ docs/.vitepress/config/footer.ts
@@ -1,6 +1,8 @@
 import { DefaultTheme } from 'vitepress/theme'
 
 export const footer: DefaultTheme.Config['footer'] = {
-    message: `Wrote with <i class="heart fa fa-heart fa-xs fa-beat"></i> and ☕ by <a href="https://toshiki.dev">Anda Toshiki</a> at <code>root@andatoshiki:/~</code> <i class="fa fa-clock fa-xs fa-beat"></i>`,
-    copyright: `Copyright © 2023-${new Date().getFullYear()} <a href="https://github.com/andatoshiki">Anda Toshiki</a> \n <span id="runtime_span"></span>`,
+    message: `Wrote with <i class="heart fa fa-heart fa-xs fa-beat"></i> and ☕ by <a href="https://toshiki.dev">Anda Toshiki</a> at <code>root@andatoshiki:/~</code>`,
+    copyright: `Copyright © 2023-${new Date().getFullYear()} <a href="https://github.com/andatoshiki">Anda Toshiki</a>
+                <br />
+                <span id="siteruntime_span"></span>`,
 }
