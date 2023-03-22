4f7693bae4eff39f57c7d3509c7fe3e7a88e71fa
andatoshiki <101481353+andatoshiki@users.noreply.github.com>
2023-03-22 15:37:56 +0800

    chore: update `prettier` config for lint files with lf for line ending

--- prettier.config.js
+++ prettier.config.js
@@ -6,6 +6,7 @@ module.exports = {
   trailingComma: 'es5',
   singleQuote: true,
   arrowParens: 'avoid',
+  endOfLine: "lf",
   overrides: [
     {
       files: '{*.md,.prettierrc,.stylelintrc,.babelrc,.html}',
