2de948fba36a3946b1d6d0c46db15b455b2c6e70
andatoshiki <101481353+andatoshiki@users.noreply.github.com>
2023-03-24 12:30:43 +0800

    fix: rearrange placement of footer text into a pyramid for aesthetic

--- docs/.vitepress/config/footer.ts
+++ docs/.vitepress/config/footer.ts
@@ -1,8 +1,6 @@
 import { DefaultTheme } from 'vitepress/theme'
 
 export const footer: DefaultTheme.Config['footer'] = {
-    message: `Wrote with <i class="heart fa fa-heart fa-xs fa-beat"></i> and ☕ by <a href="https://toshiki.dev">Anda Toshiki</a> at <code>root@andatoshiki:/~</code>`,
-    copyright: `Copyright © 2023-${new Date().getFullYear()} <a href="https://github.com/andatoshiki">Anda Toshiki</a>
-                <br />
-                <span id="siteruntime_span"></span>`,
+    copyright: `Copyright © 2023-${new Date().getFullYear()} <a href="https://github.com/andatoshiki">Anda Toshiki</a>, <a href="https://github.com/lolilab">LoliLab</a> and <a href="https://github.com/toshikidev">Toshiki Dev</a> present <br /><span id="siteruntime_span"></span>`,
+    message: `Wrote with <i class="heart fa fa-heart fa-xs fa-beat"></i> and <i class="coffee fa fa-coffee fa-xs" aria-hidden="true"></i> by <a href="https://toshiki.dev">Anda Toshiki</a> at <code>root@andatoshiki:/~</code>`,
 }
