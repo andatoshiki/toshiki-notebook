788c1bffb49c4a46e2888bc626f9573c9cfcf82b
andatoshiki <hello@toshiki.dev>
2023-05-22 19:43:34 +0800

    feat(sitemap): add feature to generate sitemap after build

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig, PageData } from 'vitepress'
 import { customElements } from './config/customElements'
 import { markdown } from './config/markdown'
 import { metaData } from './config/constants'
 import { head } from './config/head'
 import { themeConfig } from './config/theme'
+
+import { SitemapStream } from 'sitemap'
+import { createWriteStream } from 'node:fs'
+import { resolve } from 'node:path'
+
+const links: { url: string; lastmod: PageData['lastUpdated'] }[] = []
 
 export default defineConfig({
     lang: metaData.lang, // i18n default english translation
@@ -23,7 +29,22 @@ export default defineConfig({
             },
         },
     },
-    ignoreDeadLinks: true
+    ignoreDeadLinks: true,
+    transformHtml: (_, id, { pageData }) => {
+        if (!/[\\/]404\.html$/.test(id))
+          links.push({
+            url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
+            lastmod: pageData.lastUpdated
+          })
+      },
+      buildEnd: async ({ outDir }) => {
+        const sitemap = new SitemapStream({ hostname: 'https://note.toshiki.dev/' })
+        const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
+        sitemap.pipe(writeStream)
+        links.forEach((link) => sitemap.write(link))
+        sitemap.end()
+        await new Promise((r) => writeStream.on('finish', r))
+      }
 })
 
 customElements // custom element tags of markdown-it-katex in vitepress
