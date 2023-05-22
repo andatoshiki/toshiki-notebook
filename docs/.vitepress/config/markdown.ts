51d5fc7f0c4be76fc10f0e03d06ecf2a62c45639
andatoshiki <hello@toshiki.dev>
2023-05-22 11:41:34 +0800

    feat: add support for md caption & link preview & tasklist display support

--- docs/.vitepress/config/markdown.ts
+++ docs/.vitepress/config/markdown.ts
@@ -1,8 +1,11 @@
 import type { MarkdownOptions } from 'vitepress'
 import mdkatex from '@andatoshiki/markdown-it-katex'
 import mdTasklist from 'markdown-it-task-lists'
+import mdCaption from '@andatoshiki/markdown-it-image-caption'
+import mdLink from 'markdown-it-link-preview'
 
 export const markdown: MarkdownOptions = {
+    html: true,
     theme: {
         light: 'one-dark-pro',
         dark: 'material-theme-palenight',
@@ -10,6 +13,6 @@ export const markdown: MarkdownOptions = {
     lineNumbers: true,
     config: md => {
         // use more markdown-it plugins!
-        md.use(mdkatex), md.use(mdTasklist)
+        md.use(mdkatex), md.use(mdTasklist), md.use(mdCaption), md.use(mdLink)
     },
 }
