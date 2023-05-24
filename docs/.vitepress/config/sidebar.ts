4f2b400e3ecc7b54104678c8259868e11e46bf70
andatoshiki <hello@toshiki.dev>
2023-05-24 13:56:49 +0800

    chore: lint source code via prettier

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -6,7 +6,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
         {
             text: 'Wiki Database',
             collapsed: false,
-            items: [{ text: '', link: '' }],
+            items: [{ text: 'File Naming Convention', link: '/development/file-naming-convention' }],
         },
     ],
     // sidebar configs for academic related documents
@@ -16,7 +16,10 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
             text: 'Textbook',
             collapsed: true,
             items: [
-                { text: '12-5: Reaction Mechanism', link: '/academic/chemistry/notes/12-5' },
+                {
+                    text: '12-5: Reaction Mechanism',
+                    link: '/academic/chemistry/notes/12-5',
+                },
                 { text: '', link: '' },
                 { text: '', link: '' },
                 { text: '', link: '' },
@@ -30,7 +33,10 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
             text: 'Kinetics',
             collapsed: false,
             items: [
-                { text: 'Rate determining steps', link: '/academic/chemistry/notes/kinetics/rate-determining-step' },
+                {
+                    text: 'Rate determining steps',
+                    link: '/academic/chemistry/notes/kinetics/rate-determining-step',
+                },
                 // docs/academic/chemistry/notes/kinetics/rate-determining-step.md
                 { text: '', link: '' },
                 { text: '', link: '' },
@@ -42,9 +48,18 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
             collapsed: true,
             items: [
                 { text: 'Problem: 02-20', link: '/academic/chemistry/problems/02-20' },
-                { text: 'Problem: 03-02-1', link: '/academic/chemistry/problems/03-02-1' },
-                { text: 'Problem: 03-02-2', link: '/academic/chemistry/problems/03-02-2' },
-                { text: 'Problem: 03-02-3', link: '/academic/chemistry/problems/03-02-3' },
+                {
+                    text: 'Problem: 03-02-1',
+                    link: '/academic/chemistry/problems/03-02-1',
+                },
+                {
+                    text: 'Problem: 03-02-2',
+                    link: '/academic/chemistry/problems/03-02-2',
+                },
+                {
+                    text: 'Problem: 03-02-3',
+                    link: '/academic/chemistry/problems/03-02-3',
+                },
             ],
         },
     ],
@@ -56,18 +71,33 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
             collapsed: false,
             items: [
                 { text: '1: 数学', link: '/academic/physics/ipho-formulas-jpn/1' },
-                { text: '2: 一般的な推奨事', link: '/academic/physics/ipho-formulas-jpn/2' },
+                {
+                    text: '2: 一般的な推奨事',
+                    link: '/academic/physics/ipho-formulas-jpn/2',
+                },
                 { text: '3: 運動学', link: '/academic/physics/ipho-formulas-jpn/3' },
                 { text: '4: 力学', link: '/academic/physics/ipho-formulas-jpn/4' },
                 { text: '5: 振動と波', link: '/academic/physics/ipho-formulas-jpn/5' },
-                { text: '6: 幾何光学，測光', link: '/academic/physics/ipho-formulas-jpn/6' },
+                {
+                    text: '6: 幾何光学，測光',
+                    link: '/academic/physics/ipho-formulas-jpn/6',
+                },
                 { text: '7: 波動光学', link: '/academic/physics/ipho-formulas-jpn/7' },
                 { text: '8: 電気回路', link: '/academic/physics/ipho-formulas-jpn/8' },
                 { text: '9: 電磁気学', link: '/academic/physics/ipho-formulas-jpn/9' },
                 { text: '10: 熱力', link: '/academic/physics/ipho-formulas-jpn/10' },
-                { text: '11: 量子力学', link: '/academic/physics/ipho-formulas-jpn/11' },
-                { text: '12: Keplerの法則', link: '/academic/physics/ipho-formulas-jpn/12' },
-                { text: '13: 相対性理論', link: '/academic/physics/ipho-formulas-jpn/13' },
+                {
+                    text: '11: 量子力学',
+                    link: '/academic/physics/ipho-formulas-jpn/11',
+                },
+                {
+                    text: '12: Keplerの法則',
+                    link: '/academic/physics/ipho-formulas-jpn/12',
+                },
+                {
+                    text: '13: 相対性理論',
+                    link: '/academic/physics/ipho-formulas-jpn/13',
+                },
             ],
         },
     ],
@@ -116,10 +146,22 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
             text: 'Outliers',
             collapsed: true,
             items: [
-                { text: 'Introduction & Chapter 1: The Roseto Mystery', link: '/save/reading/outliers/1' },
-                { text: 'Chapter 2: The 10,000-Hour Rule', link: '/save/reading/outliers/2' },
-                { text: 'Chapter 3: The Trouble with Geniuses, Part 1', link: '/save/reading/outliers/3' },
-                { text: 'Chapter 4: The Trouble with Geniuses, Part 2', link: '/save/reading/outliers/4' },
+                {
+                    text: 'Introduction & Chapter 1: The Roseto Mystery',
+                    link: '/save/reading/outliers/1',
+                },
+                {
+                    text: 'Chapter 2: The 10,000-Hour Rule',
+                    link: '/save/reading/outliers/2',
+                },
+                {
+                    text: 'Chapter 3: The Trouble with Geniuses, Part 1',
+                    link: '/save/reading/outliers/3',
+                },
+                {
+                    text: 'Chapter 4: The Trouble with Geniuses, Part 2',
+                    link: '/save/reading/outliers/4',
+                },
                 // { text: 'Chapter 5: ', link: '/save/reading/outliers/5' },
                 // { text: 'Chapter 6: ', link: '/save/reading/outliers/6' },
             ],
@@ -131,9 +173,18 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
             text: 'markdown-it-katex',
             collapsed: false,
             items: [
-                { text: '1: How to use?', link: '/application/markdown-it-katex/how-to-use' },
-                { text: '2: KaTeX supported functions', link: '/application/markdown-it-katex/support-function' },
-                { text: '3: KaTeX support tables', link: '/application/markdown-it-katex/support-table' },
+                {
+                    text: '1: How to use?',
+                    link: '/application/markdown-it-katex/how-to-use',
+                },
+                {
+                    text: '2: KaTeX supported functions',
+                    link: '/application/markdown-it-katex/support-function',
+                },
+                {
+                    text: '3: KaTeX support tables',
+                    link: '/application/markdown-it-katex/support-table',
+                },
                 { text: '4: Tips', link: '/application/markdown-it-katex/tips' },
                 // { text: 'Chapter 5: ', link: '/save/reading/outliers/5' },
                 // { text: 'Chapter 6: ', link: '/save/reading/outliers/6' },
