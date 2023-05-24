9f56fd21bd51da24ff4fe58ed2e27b48e264bc7b
andatoshiki <hello@toshiki.dev>
2023-05-24 13:51:14 +0800

    chore: add cardlink profile short intro vue component

---
+++ docs/.vitepress/theme/components/CardLink.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="shareCard">
+        <h1 class="title">
+            <span>ようこそ👋、安田俊樹です!</span>
+        </h1>
+        <p class="description">💖 世界一番可愛いの魔法少女安田俊樹!</p>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
+<style scoped>
+.shareCard {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 15px;
+    text-align: center;
+}
+.title {
+    display: flex;
+    align-items: center;
+    font-size: 2.2em;
+    font-weight: bold;
+}
+.description {
+    margin: 1em 0;
+}
+</style>
