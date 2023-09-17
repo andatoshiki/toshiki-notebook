37bba7549b9b7954c89a89c59c25ddb654119ed7
@andatoshiki <hello@toshiki.dev>
2023-09-17 02:11:47 -0700

    chore: remove unused hunk of commented code in config files due to version iteration

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -53,7 +53,9 @@ export default withTwoslash(
                 })
         },
         buildEnd: async ({ outDir }) => {
-            const sitemap = new SitemapStream({ hostname: 'https://note.toshiki.dev/' })
+            const sitemap = new SitemapStream({
+                hostname: 'https://note.toshiki.dev/',
+            })
             const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
             sitemap.pipe(writeStream)
             links.forEach(link => sitemap.write(link))
@@ -62,53 +64,4 @@ export default withTwoslash(
         },
     })
 )
-//     defineConfig ({
-//     lang: metaData.lang, // i18n default english translation
-//     title: metaData.title, // title from metadata config
-//     description: metaData.description, // description from metadata config
-//     markdown: markdown, // markdown config
-//     lastUpdated: true, // whether enabling lastupdated or not
-//     head, // documentation head tag options
-//     themeConfig, // default exported theme config
-//     cleanUrls: true, // clean urls configs to remove standard genreated page file type extensions
-//     outDir: '../dist', // specify staic pages build output dir
-//     // vue template options for preventing katex build crashes
-//     vue: {
-//         template: {
-//             compilerOptions: {
-//                 isCustomElement: tag => customElements.includes(tag),
-//             },
-//         },
-//     },
-//     // i18n localization config
-//     locales: {
-//         '/': {
-//             label: 'English',
-//             lang: 'en-US',
-//         },
-//         '/jp/': {
-//             label: 'Japanese',
-//             title: 'Vue Test Utils',
-//             lang: 'jp-JP',
-//             description: 'La documentation officielle de Vue Test Utils',
-//         },
-//     },
-//     ignoreDeadLinks: true,
-//     transformHtml: (_, id, { pageData }) => {
-//         if (!/[\\/]404\.html$/.test(id))
-//             links.push({
-//                 url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
-//                 lastmod: pageData.lastUpdated,
-//             })
-//     },
-//     buildEnd: async ({ outDir }) => {
-//         const sitemap = new SitemapStream({ hostname: 'https://note.toshiki.dev/' })
-//         const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
-//         sitemap.pipe(writeStream)
-//         links.forEach(link => sitemap.write(link))
-//         sitemap.end()
-//         await new Promise(r => writeStream.on('finish', r))
-//     },
-// })
-
 customElements // custom element tags of markdown-it-katex in vitepress
