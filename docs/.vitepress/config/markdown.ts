098a913bea7f9a6cbac6e623c15a3e83d33273a8
@andatoshiki <hello@toshiki.dev>
2023-09-16 21:04:35 -0700

    feat(markdown): add additional markdown config for documentation on instering new custom components slots udner speciified markdown attributes and tags

--- docs/.vitepress/config/markdown.ts
+++ docs/.vitepress/config/markdown.ts
@@ -14,5 +14,11 @@ export const markdown: MarkdownOptions = {
     config: md => {
         // use more markdown-it plugins!
         md.use(mdkatex), md.use(mdTasklist), md.use(mdCaption), md.use(mdLink)
+        // insert specific custom vue layout template slots under a markdown tag attributes (currenlty disabled and unncessary because applied via markdowntransform component)
+        // md.renderer.rules.heading_close = (tokens, idx, options, env, slf) => {
+        //     let htmlResult = slf.renderToken(tokens, idx, options);
+        //     if (tokens[idx].tag === 'h1') htmlResult += `\n<ClientOnly><PageInfo /></ClientOnly>`;
+        //     return htmlResult;
+        //   }
     },
 }
