f904a405dfa8d64410e1faa007138ece4e0b0c2a
@andatoshiki <hello@toshiki.dev>
2023-09-16 21:50:07 -0700

    chore: introduce and add more extra dependencies to project for use of building new components when required

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,12 +1,30 @@
 import { defineConfig } from 'vite'
 import Components from 'unplugin-vue-components/vite'
+import UnoCSS from 'unocss/vite'
+import { MarkdownTransform } from './.vitepress/plugins/markdownTransform'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 export default defineConfig({
     plugins: [
         Components({
             dirs: ['.vitepress/theme/components'],
             include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+            resolvers: [
+                IconsResolver({
+                    componentPrefix: '',
+                }),
+            ],
+            dts: './.vitepress/components.d.ts',
+            transformer: 'vue3',
         }),
+        Icons({
+            compiler: 'vue3',
+            autoInstall: true,
+            defaultStyle: 'display: inline-block',
+        }),
+        UnoCSS(),
+        MarkdownTransform(),
     ],
     ssr: {
         noExternal: ['@andatoshiki/vitepress-plugin-nprogress', '@arco-design/web-vue'],
