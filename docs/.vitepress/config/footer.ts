8d8322c163ca58b27708394ff5a1c200649f133b
andatoshiki <hello@toshiki.dev>
2024-09-15 08:43:17 -0700

    chore(footer text): update footer text by changing fontawesome i class based icons to native unicode remoji to redude load time on landing page & rearrange the layout of footer text to stack from the shortest length chunk on top and gradually expand to the lengthiest

--- docs/.vitepress/config/footer.ts
+++ docs/.vitepress/config/footer.ts
@@ -1,6 +1,6 @@
 import { DefaultTheme } from 'vitepress/theme'
 
 export const footer: DefaultTheme.Config['footer'] = {
-    copyright: `Copyright © 2023-${new Date().getFullYear()} <a href="https://github.com/andatoshiki">Anda Toshiki</a>, <a href="https://github.com/lolilab">LoliLab</a> and <a href="https://github.com/toshikidev">Toshiki Dev</a> present <br /><span id="siteruntime_span"></span>`,
-    message: `Wrote with <i class="heart fa fa-heart fa-xs fa-beat"></i> and <i class="coffee fa fa-coffee fa-xs" aria-hidden="true"></i> by <a href="https://toshiki.dev">Anda Toshiki</a> at <code>root@andatoshiki:/~</code>`,
+    copyright: `Wrote with <span class="heart">💓</span> with 🌵 by <a href="https://toshiki.dev">Anda Toshiki</a> at <code>root@andatoshiki:/~</code> in the innovative HQ of <a href="https://asu.edu">ASU</a>`,
+    message: `Copyright © 2023-${new Date().getFullYear()} <a href="https://github.com/andatoshiki">Anda Toshiki</a>, <a href="https://github.com/lolilab">LoliLab</a> and <a href="https://github.com/toshikidev">Toshiki Dev</a> present <br /><span id="siteruntime_span"></span>`,
 }
