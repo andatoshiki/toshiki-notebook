2f8c38f55edd286a34bc1e7db2df0e260d2b6908
andatoshiki <hello@toshiki.dev>
2024-09-15 09:43:42 -0700

    feat(confetti): add vue confetti component utility source to vitepress component directory and add registered component to index landing page of vitepress note site

---
+++ docs/.vitepress/theme/components/Confetti.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import confetti from 'canvas-confetti'
+import { inBrowser } from 'vitepress'
+
+if (inBrowser) {
+    /* confetti */
+    confetti({
+        particleCount: 100,
+        spread: 170,
+        origin: { y: 0.6 },
+    })
+}
+</script>
